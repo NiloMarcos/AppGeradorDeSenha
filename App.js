4a0e4f67a1984b4d09f853227c422a07130c352c
Nilo Marcos <nilo.n2m15@gmail.com>
2021-03-31 15:06:15 -0300

    App modificado

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert} from 'react-native';
+import Slider from '@react-native-community/slider';
 
-export default function App() {
-  return (
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+
+export default function App(){
+  const [password, setPassword] = useState('');
+  const  [size, setSize] = useState(10);
+
+  function gerarSenha(){
+    let pass = '';
+    for(let i = 0, n = charset.length; i < size; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    setPassword(pass);
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./src/assets/logo.png')} style={styles.logo}/>
+
+      <Text style={styles.title}>{size} Caracteres</Text>
+    
+      <View style={styles.area}>
+        <Slider style={{height: 50}} minimumValue={5} 
+                                     maximumValue={15} 
+                                     minimumTrackTintColor="#FF0000" 
+                                     maximumTrackTintColor="#000"
+                                     value={size}
+                                     onValueChange={ (valor) => setSize(valor)}
+                                     />
+      </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => gerarSenha()}>
+          <Text style={styles.buttonText}>Gerar Senha</Text>
+        </TouchableOpacity>
+
+        {password !== '' && (
+          <View style={styles.area}>
+            <Text style={styles.passworld}>{password}</Text>
+          </View>
+        )}
     </View>
   );
 }
@@ -14,8 +49,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000'
   },
-});
+  logo: {
+    marginBottom: 60
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF'
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#A9A9A9',
+    width: '80%',
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A9A9A9',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold'
+  },
+  passworld: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 25
+  }
+})
